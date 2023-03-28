978780a5b95b167621f910845d6d838075f426ee
Adam Smith <adamrsmith98@gmail.com>
2023-03-28 10:57:59 -0600

    test

--- handler.py
+++ handler.py
@@ -9,27 +9,15 @@ import numpy as np
 import boto3
 from botocore.config import Config
 
-from nav import Nav
+# from nav import Nav
 
-GRAVITY = 9.80665  # m / s ** 2
+# GRAVITY = 9.80665  # m / s ** 2
 
 client = boto3.client(
     'apigatewaymanagementapi',
     endpoint_url="https://98ldqkpb7k.execute-api.us-east-1.amazonaws.com/dev",
     config=Config(tcp_keepalive=True),
 )
-
-
-class Counter:
-    def __init__(self) -> None:
-        self.count = 0
-
-    def iterate(self):
-        self.count += 1
-        return self.count
-
-
-counter = Counter()
 
 
 def websocket_handler(event, context):
@@ -43,6 +31,9 @@ def websocket_handler(event, context):
 
         connectionId = event.get('requestContext', {}).get('connectionId')
         message = event.get('body', {})
+
+        # if 'nav' not in globals():
+        #     set_nav(displacement, is_supercharged, drag_coeff)
 
         payload = {
             'fuel': np.random.rand(),
@@ -60,52 +51,56 @@ def websocket_handler(event, context):
         return {'statusCode': 400, 'body': 'Unknown WebSocket event'}
 
 
-def set_nav(displacement=None, is_supercharged=None, drag_coeff=None):
-    global nav
-    nav = Nav(
-        smoothing_critical_freq=0.03,
-        vz_depth=3,
-        period=0.01,
-        algo='madgwick',
-        smooth_fc=True,
-        fc_smoothing_critical_freq=0.02,
-        imu_damping=0.05,
-        fc_reduction_factor=0.5,
-        displacement=displacement,
-        is_supercharged=is_supercharged,
-        drag_coeff=drag_coeff,
-    )
+# def set_nav(displacement=None, is_supercharged=None, drag_coeff=None):
+#     global nav
+#     nav = Nav(
+#         smoothing_critical_freq=0.03,
+#         vz_depth=3,
+#         period=0.01,
+#         algo='madgwick',
+#         smooth_fc=True,
+#         fc_smoothing_critical_freq=0.02,
+#         imu_damping=0.05,
+#         fc_reduction_factor=0.5,
+#         displacement=displacement,
+#         is_supercharged=is_supercharged,
+#         drag_coeff=drag_coeff,
+#     )
 
 
-def set_params(displacement, is_supercharged, drag_coeff=None):
-    if 'nav' not in globals():
-        print('WARNING: Navigation not initialized. Call set_nav to initialize.')
-        return
-    nav.set_vehicle_params(displacement, is_supercharged, drag_coeff)
+# def set_params(displacement, is_supercharged, drag_coeff=None):
+#     if 'nav' not in globals():
+#         print('WARNING: Navigation not initialized. Call set_nav to initialize.')
+#         return
+#     nav.set_vehicle_params(displacement, is_supercharged, drag_coeff)
 
 
-def run_nav(batch):
-    if 'nav' not in globals():
-        print('WARNING: Navigation not initialized. Call set_nav to initialize.')
-        return
+# def run_nav(batch):
+#     if 'nav' not in globals():
+#         print('WARNING: Navigation not initialized. Call set_nav to initialize.')
+#         return
 
-    for data in batch:
-        t, ax, ay, az, ax_nog, ay_nog, az_nog, gx, gy, gz, mx, my, mz, lat, long, alt, heading, speed = data
+#     for data in batch:
+#         t, ax, ay, az, ax_nog, ay_nog, az_nog, gx, gy, gz, mx, my, mz, lat, long, alt, heading, speed = data
 
-        acc = np.array([ax * GRAVITY, ay * GRAVITY, az * GRAVITY])
-        acc_nog = np.array([ax_nog * GRAVITY, ay_nog * GRAVITY, az_nog * GRAVITY])
-        gyr = np.array([gx, gy, gz])
-        mag = np.array([mx, my, mz])
+#         acc = np.array([ax * GRAVITY, ay * GRAVITY, az * GRAVITY])
+#         acc_nog = np.array([ax_nog * GRAVITY, ay_nog * GRAVITY, az_nog * GRAVITY])
+#         gyr = np.array([gx, gy, gz])
+#         mag = np.array([mx, my, mz])
 
-        nav.process_imu_update(t, acc, acc_nog, gyr, mag)
-        if lat is not None:
-            nav.process_gps_update(t, lat, long, alt, heading, speed)
+#         nav.process_imu_update(t, acc, acc_nog, gyr, mag)
+#         if lat is not None:
+#             nav.process_gps_update(t, lat, long, alt, heading, speed)
 
-    return nav.get_fuel_and_emissions()
+#     fuel = nav.get_fuel(return_totals=False)
+#     emissions = nav.get_emissions(return_totals=False)
+#     motion = nav.get_motion()
+
+#     return fuel | emissions
 
 
-def end_nav():
-    global nav
-    trip_metrics = nav.get_trip_metrics()
-    del nav
-    return trip_metrics
+# def end_nav():
+#     global nav
+#     trip_metrics = nav.get_trip_metrics()
+#     del nav
+#     return trip_metrics
