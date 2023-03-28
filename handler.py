852c45ab24432624c18506a07b1687fee6ab9c57
Adam Smith <adamrsmith98@gmail.com>
2023-03-28 12:47:51 -0600

    update handler

--- handler.py
+++ handler.py
@@ -3,7 +3,6 @@ try:
 except ImportError:
     pass
 
-
 import json
 import numpy as np
 import boto3
@@ -12,6 +11,7 @@ from botocore.config import Config
 from nav import Nav
 
 GRAVITY = 9.80665  # m / s ** 2
+DEG_TO_RAD = np.pi / 180
 
 client = boto3.client(
     'apigatewaymanagementapi',
@@ -33,7 +33,52 @@ def websocket_handler(event, context):
         message = event.get('body', {})
 
         # if 'nav' not in globals():
-        #     set_nav(displacement, is_supercharged, drag_coeff)
+        #     set_nav(message['displacement'], message['isSupercharged'], message['drag'])
+
+        # acc = np.array(
+        #     [
+        #         message['accelerometerWithGravity']['x'],
+        #         message['accelerometerWithGravity']['y'],
+        #         message['accelerometerWithGravity']['z'],
+        #     ]
+        # )
+
+        # acc_nog = np.array(
+        #     [
+        #         message['accelerometerWithoutGravity']['x'],
+        #         message['accelerometerWithoutGravity']['y'],
+        #         message['accelerometerWithoutGravity']['z'],
+        #     ]
+        # )
+
+        # gyro = np.array(
+        #     [
+        #         message['gyroscope']['beta'] * DEG_TO_RAD,
+        #         message['gyroscope']['gamma'] * DEG_TO_RAD,
+        #         message['gyroscope']['alpha'] * DEG_TO_RAD,
+        #     ]
+        # )
+
+        # mag = np.array(
+        #     [
+        #         message['magnetometer']['x'],
+        #         message['magnetometer']['y'],
+        #         message['magnetometer']['z'],
+        #     ]
+        # )
+
+        # payload = run_nav(
+        #     message['time'],
+        #     acc,
+        #     acc_nog,
+        #     gyro,
+        #     mag,
+        #     message['lat'],
+        #     message['lon'],
+        #     message['alt'],
+        #     message['heading'],
+        #     message['speed'],
+        # )
 
         payload = {
             'fuel': np.random.rand(),
@@ -51,56 +96,48 @@ def websocket_handler(event, context):
         return {'statusCode': 400, 'body': 'Unknown WebSocket event'}
 
 
-# def set_nav(displacement=None, is_supercharged=None, drag_coeff=None):
-#     global nav
-#     nav = Nav(
-#         smoothing_critical_freq=0.03,
-#         vz_depth=3,
-#         period=0.01,
-#         algo='madgwick',
-#         smooth_fc=True,
-#         fc_smoothing_critical_freq=0.02,
-#         imu_damping=0.05,
-#         fc_reduction_factor=0.5,
-#         displacement=displacement,
-#         is_supercharged=is_supercharged,
-#         drag_coeff=drag_coeff,
-#     )
+def set_nav(displacement=None, is_supercharged=None, drag_coeff=None):
+    global nav
+    nav = Nav(
+        smoothing_critical_freq=0.03,
+        vz_depth=3,
+        period=0.01,  ############################ UPDATE THIS #######################################
+        algo='madgwick',
+        smooth_fc=True,
+        fc_smoothing_critical_freq=0.02,
+        imu_damping=0.05,
+        fc_reduction_factor=0.5,
+        displacement=displacement,
+        is_supercharged=is_supercharged,
+        drag_coeff=drag_coeff,
+    )
 
 
-# def set_params(displacement, is_supercharged, drag_coeff=None):
-#     if 'nav' not in globals():
-#         print('WARNING: Navigation not initialized. Call set_nav to initialize.')
-#         return
-#     nav.set_vehicle_params(displacement, is_supercharged, drag_coeff)
+def set_params(displacement, is_supercharged, drag_coeff=None):
+    if 'nav' not in globals():
+        print('WARNING: Navigation not initialized. Call set_nav to initialize.')
+        return
+    nav.set_vehicle_params(displacement, is_supercharged, drag_coeff)
 
 
-# def run_nav(batch):
-#     if 'nav' not in globals():
-#         print('WARNING: Navigation not initialized. Call set_nav to initialize.')
-#         return
+def run_nav(t, acc, acc_nog, gyro, mag, lat, long, alt, heading, speed):
+    if 'nav' not in globals():
+        print('WARNING: Navigation not initialized. Call set_nav to initialize.')
+        return
 
-#     for data in batch:
-#         t, ax, ay, az, ax_nog, ay_nog, az_nog, gx, gy, gz, mx, my, mz, lat, long, alt, heading, speed = data
+    nav.process_imu_update(t, acc, acc_nog, gyro, mag)
+    if lat is not None:
+        nav.process_gps_update(t, lat, long, alt, heading, speed)
 
-#         acc = np.array([ax * GRAVITY, ay * GRAVITY, az * GRAVITY])
-#         acc_nog = np.array([ax_nog * GRAVITY, ay_nog * GRAVITY, az_nog * GRAVITY])
-#         gyr = np.array([gx, gy, gz])
-#         mag = np.array([mx, my, mz])
+    fuel = nav.get_fuel(return_totals=False)
+    emissions = nav.get_emissions(return_totals=False)
+    speed = nav.get_motion(speed_only=True)
 
-#         nav.process_imu_update(t, acc, acc_nog, gyr, mag)
-#         if lat is not None:
-#             nav.process_gps_update(t, lat, long, alt, heading, speed)
-
-#     fuel = nav.get_fuel(return_totals=False)
-#     emissions = nav.get_emissions(return_totals=False)
-#     speed = nav.get_motion(speed_only=True)
-
-#     return fuel | emissions | speed
+    return {**fuel, **emissions, **speed}
 
 
-# def end_nav():
-#     global nav
-#     trip_metrics = nav.get_trip_metrics()
-#     del nav
-#     return trip_metrics
+def end_nav():
+    global nav
+    trip_metrics = nav.get_trip_metrics()
+    del nav
+    return trip_metrics
