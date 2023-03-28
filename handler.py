dc26eab6ce7ee325baf7d3a199234b1ce9513d2f
wongsitu <sa_waika_4d_23@hotmail.com>
2023-03-27 19:52:37 -0600

    api gateway

--- handler.py
+++ handler.py
@@ -20,8 +20,8 @@ def websocket_handler(event, context):
     elif route == '$disconnect':
         return {'statusCode': 200 }
     elif route == '$default':
-        connectionId = event['connectionId']
-        client.post_to_connection(ConnectionId = connectionId, Data = msg.encode(('utf-8')))
+        # connectionId = event['connectionId']
+        # client.post_to_connection(ConnectionId = connectionId, Data = msg.encode(('utf-8')))
 
         message = event.get('body')
         response = {'message': 'Received message: {}'.format(message)}
